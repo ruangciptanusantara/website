7b8296c0c657110e34ad864454324837754f5ea7
ruangciptanusantara <ruangciptanusantara@gmail.com>
2024-10-26 09:46:46 +0700

    Add files via upload

---
+++ js/include-html.js
@@ -0,0 +1,49 @@
+const navbar_fixed = document.querySelector('.include-navbar_fixed')
+fetch('navbar_fixed.html')
+.then(res=>res.text())
+.then(data=>{
+    navbar_fixed.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script'))
+})
+
+const navbar = document.querySelector('.include-navbar')
+fetch('navbar.html')
+.then(res=>res.text())
+.then(data=>{
+    navbar.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script'))
+})
+
+const footer = document.querySelector('.include-footer')
+fetch('footer.html')
+.then(res=>res.text())
+.then(data=>{
+    footer.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script'))
+})
+
+const copyright = document.querySelector('.include-copyright')
+fetch('copyright.html')
+.then(res=>res.text())
+.then(data=>{
+    copyright.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script'))
+})
+
+const loader = document.querySelector('.include-loader')
+fetch('loader.html')
+.then(res=>res.text())
+.then(data=>{
+    loader.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script'))
+})
